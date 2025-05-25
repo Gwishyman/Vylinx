1f0817c224885a1300a7030e9f4f86f43d56fcb4
Gwishyman <153815161+Gwishyman@users.noreply.github.com>
2025-05-25 19:20:52 +1000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBHY0RBY3jK7ZnuM2anIbKzFDd65xwMiyg",
   authDomain: "vylinx-11e87.firebaseapp.com",
   projectId: "vylinx-11e87",
-  storageBucket: "vylinx-11e87.firebasestorage.app",
+  storageBucket: "vylinx-11e87.appspot.com",
   messagingSenderId: "710300713760",
   appId: "1:710300713760:web:c4ee8f53be614f5869cd45"
 };
@@ -15,4 +15,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, auth, db };
+export { auth, db };
